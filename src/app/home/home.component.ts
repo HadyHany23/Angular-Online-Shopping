943593ab3092c65f9b5927b1a8170f20457c3d24
Hady Hany <hadyharb1988@gmail.com>
2024-06-02 12:55:37 +0300

    Edits

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SliderComponent } from '../slider/slider.component';
 import { ProductItemComponent } from '../product-item/product-item.component';
+import { InnerHeaderComponent } from '../inner-header/inner-header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet,SliderComponent,ProductItemComponent],
+  imports: [RouterOutlet,SliderComponent,ProductItemComponent,InnerHeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
